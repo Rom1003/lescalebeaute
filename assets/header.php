57b7cc9d7d6c192c0150efd6068ffda1a76db997
Rom1003 <s-rom1@live.fr>
2018-02-07 23:36:27 +0100

    Création du loader

--- assets/header.php
+++ assets/header.php
@@ -6,35 +6,43 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foundation for Sites</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="assets/css/foundation.css">
+    <link rel="stylesheet" href="assets/css/base.css">
 </head>
 
 <body>
 
-    <div class="page-loader none">
-        CHARGEMENT
+    <div class="page-loader">
+        <div id="loader-content">
+            <div id="loader-img">
+                <div id="loader-img-border"></div>
+                <img src="assets/img/logoFondBlanc.svg" alt="l'escale beauté" />
+            </div>
+            
+            <p id="loader-text1">L'escale beauté se prépare ...</p>
+            <p id="loader-text2">Veuillez patienter ...</p>
+        </div>
     </div>
 
     <div class="top-bar">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text">Site Title</li>
-      <li>
-        <a href="#">One</a>
-        <ul class="menu vertical">
-          <li><a href="#">One</a></li>
-          <li><a href="#">Two</a></li>
-          <li><a href="#">Three</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Two</a></li>
-      <li><a href="#">Three</a></li>
-    </ul>
-  </div>
-  <div class="top-bar-right">
-    <ul class="menu">
+        <div class="top-bar-left">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li class="menu-text">Site Title</li>
+                <li>
+                    <a href="#">One</a>
+                    <ul class="menu vertical">
+                        <li><a href="#">One</a></li>
+                        <li><a href="#">Two</a></li>
+                        <li><a href="#">Three</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Two</a></li>
+                <li><a href="#">Three</a></li>
+            </ul>
+        </div>
+        <div class="top-bar-right">
+            <ul class="menu">
 
-    </ul>
-  </div>
-</div>
+            </ul>
+        </div>
+    </div>
