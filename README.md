e2afc3929211e65e413df7c74c113c0940c4368d
Rom1003 <romain.simon1@etudiant.univ-reims.fr>
2018-02-18 20:23:53 +0100

    Ajout de gulp

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Création du site pour "L'escale beauté"
+
+Pré-requis : 
+ - Node.js 
+    -- Gulp : npm install --global gulp-cli
+ - composer
+ 
+ 
+
+  
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('workflow', function () {
+    gulp.src('./src/css/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9', 'Android >= 2.3', 'ios >= 7'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./'))
+
+        .pipe(gulp.dest('./src/css/'))
+});
+
+gulp.task('default', function () {
+    gulp.watch('./src/css/**/*.scss', ['workflow']);
+});
